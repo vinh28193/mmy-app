a966fe35c1e3d6014c4bd957407989de4c920931
Vũ Vinh <vinhvv@peacesoft.net>
2017-11-24 15:11:38 +0700

    Todo Service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
+interface Res {
+    susscess: boolean;
+    messages: string;
+    data: any;
+}
 @Injectable()
 export class AppService {
-
-    constructor() { }
+    public messages = [];
+    protected options;
+    constructor(private http: HttpClient) { }
+    protected get(url: string): Observable<any> {
+        return this.http.get(url, this.options);
+    }
+    protected post(url: string, body: any | null) {
+        return this.http.post(url, body, this.options);
+    }
 }
