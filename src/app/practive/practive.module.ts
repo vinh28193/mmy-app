96c0e612b09480904073ab1e3d7217294df7af5d
Vũ Vinh <vinhvv@peacesoft.net>
2017-12-28 11:24:19 +0700

    add in memory service

--- src/app/practive/practive.module.ts
+++ src/app/practive/practive.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from '../in-memory-data.service';
+
 import { PostsComponent } from './posts/posts.component';
 import { PractiveRoutingModule } from './practive-routing.module';
 import { PostsService } from './posts/posts.service';
@@ -11,6 +15,8 @@ import { PostsService } from './posts/posts.service';
   imports: [
       CommonModule,
       PractiveRoutingModule,
+      HttpClientModule,
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 1500 })
   ],
   declarations: [PostsComponent],
   providers: [PostsService]
